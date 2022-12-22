a3cedbc8322c57430adf746c91fd42a837cc3083
Joseph Uwagbale <joecrypt@yahoo.com>
2022-12-22 19:55:12 +0000

    changing lower to upper case2

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 def uppercase(str):
-    l = len(str)
+    lent = len(str)
     count = 0
-    for i in str:
-        c = ord(i)
-        count += 1
-        if c >= 97 and c <= 122:
-            c -= 32
-        if count != l:
-            print("{:c}".format(c), end='')
-        else:
-            print("{:c}".format(c))
+    if str:
+        for i in str:
+            c = ord(i)
+            count += 1
+            if c >= 97 and c <= 122:
+                c -= 32
+            if count != lent:
+                print("{:c}".format(c), end='')
+            else:
+                print("{:c}".format(c))
